cfaf08e1ee19fd5ee0f62ef18e284d376263d3b5
DFIT <nobab.yousuf.hazi.99@gmail.com>
2024-09-27 11:53:35 +0600

    comit 1

---
+++ lib/data types.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 23;
+  double pi= 3;
+  num mark = 100;
+
+  String name = 'Yousuf';
+  bool married= true;
+
+  print('My name is $name, I am $age years old and I am ${married ? 'Married':'unmarried'}');
+
+}
